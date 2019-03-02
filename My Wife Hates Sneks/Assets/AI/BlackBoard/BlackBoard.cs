f0fa3c49085569ac8d8524b2251fc32ccb4ef4bc
Donovan Bergin <kaiserbergin@gmail.com>
2019-03-02 10:36:13 -0600

    Added blackboard tests and clear method

--- My Wife Hates Sneks/Assets/AI/BlackBoard/BlackBoard.cs
+++ My Wife Hates Sneks/Assets/AI/BlackBoard/BlackBoard.cs
@@ -1,40 +1,89 @@
-﻿using System.Collections;
+﻿using System;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BlackBoard : MonoBehaviour
+namespace Assets.AI.Blackboard
 {
-    public static BlackBoard instance = null;
-
-    private void Awake()
+    public class BlackBoard
     {
-        if (instance == null) instance = this;
-        else if (instance != null) Destroy(gameObject);
+        private static readonly Lazy<BlackBoard> lazy = new Lazy<BlackBoard>(() => new BlackBoard());
 
-        DontDestroyOnLoad(gameObject);
+        public static BlackBoard Instance { get { return lazy.Value; } }
+
+        private BlackBoard()
+        {
+            Objects = new Dictionary<string, object>();
+            GameObjects = new Dictionary<string, GameObject>();
+            Transforms = new Dictionary<string, Transform>();
+            Vectors = new Dictionary<string, Vector3>();
+            ScriptableObjects = new Dictionary<string, ScriptableObject>();
+
+            Strings = new Dictionary<string, string>();
+            Floats = new Dictionary<string, float>();
+            Ints = new Dictionary<string, int>();
+            Bools = new Dictionary<string, bool>();
+
+            ObjectLists = new Dictionary<string, List<object>>();
+            GameObjectLists = new Dictionary<string, List<GameObject>>();
+            TransformLists = new Dictionary<string, List<Transform>>();
+            VectorLists = new Dictionary<string, List<Vector3>>();
+            ScriptableObjectLists = new Dictionary<string, List<ScriptableObject>>();
+
+            StringLists = new Dictionary<string, List<string>>();
+            FloatLists = new Dictionary<string, List<float>>();
+            IntLists = new Dictionary<string, List<int>>();
+            BoolLists = new Dictionary<string, List<bool>>();
+        }
+
+        public void ClearBlackBoard()
+        {
+            Objects.Clear();
+            GameObjects.Clear();
+            Transforms.Clear();
+            Vectors.Clear();
+            ScriptableObjects.Clear();
+
+            Strings.Clear();
+            Floats.Clear();
+            Ints.Clear();
+            Bools.Clear();
+
+            ObjectLists.Clear();
+            GameObjectLists.Clear();
+            TransformLists.Clear();
+            VectorLists.Clear();
+            ScriptableObjectLists.Clear();
+
+            StringLists.Clear();
+            FloatLists.Clear();
+            IntLists.Clear();
+            BoolLists.Clear();
+        }
+
+        // Single Items
+        public Dictionary<string, object> Objects;
+        public Dictionary<string, GameObject> GameObjects;
+        public Dictionary<string, Transform> Transforms;
+        public Dictionary<string, Vector3> Vectors;
+        public Dictionary<string, ScriptableObject> ScriptableObjects;
+
+        public Dictionary<string, string> Strings;
+        public Dictionary<string, float> Floats;
+        public Dictionary<string, int> Ints;
+        public Dictionary<string, bool> Bools;
+
+        // Item Lists
+        public Dictionary<string, List<object>> ObjectLists;
+        public Dictionary<string, List<GameObject>> GameObjectLists;
+        public Dictionary<string, List<Transform>> TransformLists;
+        public Dictionary<string, List<Vector3>> VectorLists;
+        public Dictionary<string, List<ScriptableObject>> ScriptableObjectLists;
+
+        public Dictionary<string, List<string>> StringLists;
+        public Dictionary<string, List<float>> FloatLists;
+        public Dictionary<string, List<int>> IntLists;
+        public Dictionary<string, List<bool>> BoolLists;
+
+        
     }
-
-    // Single Items
-    public Dictionary<string, object> Objects = new Dictionary<string, object>();
-    public Dictionary<string, GameObject> GameObjects = new Dictionary<string, GameObject>();    
-    public Dictionary<string, Transform> Transforms = new Dictionary<string, Transform>();
-    public Dictionary<string, Vector3> Vectors = new Dictionary<string, Vector3>();
-    public Dictionary<string, ScriptableObject> ScriptableObjects = new Dictionary<string, ScriptableObject>();
-
-    public Dictionary<string, string> Strings = new Dictionary<string, string>();
-    public Dictionary<string, float> Floats = new Dictionary<string, float>();
-    public Dictionary<string, int> Ints = new Dictionary<string, int>();
-    public Dictionary<string, bool> Bools = new Dictionary<string, bool>();
-
-    // Item Lists
-    public Dictionary<string, List<object>> ObjectLists = new Dictionary<string, List<object>>();
-    public Dictionary<string, List<GameObject>> GameObjectLists = new Dictionary<string, List<GameObject>>();
-    public Dictionary<string, List<Transform>> TransformLists = new Dictionary<string, List<Transform>>();
-    public Dictionary<string, List<Vector3>> VectorLists = new Dictionary<string, List<Vector3>>();
-    public Dictionary<string, List<ScriptableObject>> ScriptableObjectLists = new Dictionary<string, List<ScriptableObject>>();
-
-    public Dictionary<string, List<string>> StringLists = new Dictionary<string, List<string>>();
-    public Dictionary<string, List<float>> FloatLists = new Dictionary<string, List<float>>();
-    public Dictionary<string, List<int>> IntLists = new Dictionary<string, List<int>>();
-    public Dictionary<string, List<bool>> BoolLists = new Dictionary<string, List<bool>>();
 }
---
+++ My Wife Hates Sneks/Assets/Tests/AI/Blackboard/BlackboardTests.cs
@@ -0,0 +1,45 @@
+﻿using Assets.AI.Blackboard;
+using NUnit.Framework;
+
+
+namespace Tests
+{
+    public class BlackboardTests
+    {
+        [SetUp]
+        public void SetUp()
+        {
+            BlackBoard.Instance.ClearBlackBoard();
+        }
+
+        [Test]
+        public void AccessBlackBoardTest()
+        {
+            var instance = BlackBoard.Instance;
+            Assert.NotNull(instance);
+        }
+
+        [Test]
+        public void BlackBoardSingleItemObjectTests()
+        {
+            var instance = BlackBoard.Instance;
+
+            var obj1 = new object();
+            var obj2 = new object();
+            var obj3 = new object();
+
+            instance.Objects.Add("obj1", obj1);
+            instance.Objects.Add("obj2", obj2);
+            instance.Objects.Add("obj3", obj3);
+
+            Assert.IsTrue(instance.Objects.ContainsKey("obj1"));
+            Assert.IsTrue(instance.Objects.ContainsKey("obj2"));
+            Assert.IsTrue(instance.Objects.ContainsKey("obj3"));
+            Assert.IsFalse(instance.Objects.ContainsKey("asdf"));
+
+            Assert.AreSame(instance.Objects["obj1"], obj1);
+            Assert.AreSame(instance.Objects["obj2"], obj2);
+            Assert.AreSame(instance.Objects["obj3"], obj3);
+        }
+    }
+}
