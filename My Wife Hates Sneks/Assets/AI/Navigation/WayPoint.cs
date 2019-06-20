b0dcaf2c9f14486845fe97c560064665f63151c2
Donovan Bergin <kaiserbergin@gmail.com>
2019-06-19 23:30:38 -0500

    Added nav playground

---
+++ My Wife Hates Sneks/Assets/AI/Navigation/WayPoint.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WayPoint : MonoBehaviour
+{
+    public float drawRadius = .25f;
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, drawRadius);
+    }
+}
---
+++ My Wife Hates Sneks/Assets/Waifu/WaifuMovuwu.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class WaifuMovuwu : MonoBehaviour
+{
+    public WayPoint[] WayPoints;
+    public int CurrentWayPointIndex;
+    private NavMeshAgent NavMeshAgent;
+    // Start is called before the first frame update
+    void Start()
+    {
+        NavMeshAgent = GetComponentInChildren<NavMeshAgent>();
+
+        CurrentWayPointIndex = 0;
+        NavigateToNextWayPoint();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (ArrivedAtWaypoint())
+        {
+            SetNextWayPointIndex();
+            NavigateToNextWayPoint();
+        }
+    }
+
+    private void NavigateToNextWayPoint()
+    {
+        NavMeshAgent.SetDestination(WayPoints[CurrentWayPointIndex].transform.position);
+    }
+
+    private bool ArrivedAtWaypoint()
+    {
+        return transform.position.x == WayPoints[CurrentWayPointIndex].transform.position.x && transform.position.z == WayPoints[CurrentWayPointIndex].transform.position.z;
+    }
+
+    private void SetNextWayPointIndex()
+    {
+        CurrentWayPointIndex = CurrentWayPointIndex == WayPoints.Length - 1 ? 0 : CurrentWayPointIndex + 1;
+    }
+}
